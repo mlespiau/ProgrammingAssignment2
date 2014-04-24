cf840b985a265f4f7dd5cefa7ad8f8e6b0d0007f
Matias Lespiau <mlespiau@avature.net>
2014-04-24 10:32:00 -0300

    Improve comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# The following pair of functions provide an API for caching solutions to matrix inverses  
+# in order to avoid computing it repeatedly. 
+# Example of usage:
+#
+# source("cachematrix.R")
+#
+# matrix <- replicate(2000, rnorm(2000)) # Create a large matrix
+# m <- makeCacheMatrix(matrix) # Wrap matrix using makeCacheMatrix
+# i <- cacheSolve(m) # Solves inverse for the first time
+# 
+# i <- cacheSolve(m) # subsequent calls will use cache and output "getting cache solution"
 
 makeCacheMatrix <- function(x = matrix()) {
+  # A matrix wrapper that can holds it's inverse.
+  # This wrapper can be used by cacheSolve function to solve the inverse or get it from the cache.
+  #
+  # Args:
+  #   x: matrix for whom we want to solve it's invese
+  #
+  # Returns:
+  #   A list with setters and getter for either the matrix or it's inverse.
+  
   i <- NULL
   set <- function(y) {
       x <<- y
@@ -16,10 +32,17 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  # Solves the inverse of 'x' by either using the solve functon or a cache.
+  # Uses the cached solution if it is available (and outputs "getting cache solution"). 
+  # If it's not cached, calculates the inverse and updates the cache.
+  # 
+  # Args:
+  #   x: matrix wrapper created by makeCacheMatrix function
+  #
+  # Returns:
+  #   A matrix that is the inverse of 'x'
+  
   i <- x$getInverse()
   if (!is.null(i)) {
     message("getting cache solution")
